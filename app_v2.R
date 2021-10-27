14f19d98162e19096276004e7bef0c333b05255b
Graf <graffs@dnr.state.co.us>
2021-10-27 11:50:33 -0600

    added html, conditional panel for downloads

--- app_v2.R
+++ app_v2.R
@@ -5,22 +5,37 @@ library(DT)
 
 
 source("clean_data_function.R")
+previous_detections <- read.csv("WGFP_Raw_20210505.csv")
+
 
 ui <- fluidPage(
+    titlePanel("WGFP Data Uploads"),
     sidebarLayout(
         sidebarPanel(
-            fileInput("file1", "Choose CSV File",
+            fileInput("file1", "Choose File",
                       # accept = c(
                       #     "text/csv",
                       #     "text/comma-separated-values,text/plain",
                       #     ".csv")
             ),
             tags$hr(),
-            checkboxInput("header", "Header", TRUE)
+            checkboxInput("header", "Header", TRUE),
+            conditionalPanel(condition = "output.fileUploaded == true",
+                             actionButton(inputId = "download1", label = "Save as CSV")
+                             ) #end of conditional panel
         ),
-        mainPanel(
-            tableOutput("contents")
-        )
+        mainPanel(tabsetPanel(
+            tabPanel("How to Use",
+                     includeHTML(paste0("www/", "WGFP_data_uploads_about.html"))),
+            tabPanel("New Detections",
+                     DT::dataTableOutput("new_data_contents")),
+            tabPanel("Previous Detections",
+                     DT::dataTableOutput("previousdata"))
+            
+            
+        ), #end of tabset panel
+            
+        ) #end of main panel
     )
 )
 
@@ -40,14 +55,32 @@ server <- function(input, output) {
         read.delim(inFile$datapath, sep = " ", na.strings=c("", "NA"),
                    #skip = 5,
                    header= FALSE)    })
+    #makes a fileUploaded output option to return back to conditional panel for saving csv
+    output$fileUploaded <- reactive({
+        return(!is.null(new_data()))
+    })
+    
+    outputOptions(output, 'fileUploaded', suspendWhenHidden=FALSE)
     
     cleaned_data <- reactive({
         clean_txt(new_data())
     })
     
-    output$contents <- renderTable({
+    output$new_data_contents <- renderDataTable({
         cleaned_data()
     })
+    
+
+# Downloading CSV ---------------------------------------------------------
+    # output$download1 <- downloadHandler(
+    #     filename = function() {
+    #         paste(input$file1, ".csv", sep = "")
+    #     },
+    #     content = function(file) {
+    #         write.csv(datasetInput(), file, row.names = FALSE)
+    #     }
+    # )
+    
 }
 
 shinyApp(ui, server)
